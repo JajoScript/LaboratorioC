12d86db2117cbff6f05288bcc048d60a1253f40d
Javier Almarza <javieralmarzabucarey@gmail.com>
2020-04-28 21:34:14 +0000

    lectura de archivos

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-  printf("Hola mundos\n");
-  return 0;
+void leerProblema(const char *nombre_archivo){
+    FILE * archivo;
+    char linea[1000],
+    delimitador[] = ",",
+    *token;
+
+    archivo = fopen(nombre_archivo, "r");
+
+    if (archivo == NULL){
+        printf( "No se puede abrir el fichero.\n" );
+        return;
+    }
+
+    while( fgets (linea, 1000, archivo) != NULL ){
+        token = strtok(linea, delimitador);
+        printf("RUT: %s\n", token);
+
+        token = strtok(NULL, delimitador);
+        printf("NOMBRE: %s\n", token);
+
+        token = strtok(NULL, delimitador);
+        printf("ENTRADAS: %s\n", token);
+        printf("-----------------------\n");
+    }
+}
+
+int main(void){
+  leerProblema("./input.csv");
 }
