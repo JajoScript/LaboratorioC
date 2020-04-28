bd9ec6ad1820cc34c6bbe93cf278fe8c349354fa
Vicente Salas <speed.mta9@gmail.com>
2020-04-28 23:58:03 +0000

    Creación de Listas enlazadas

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ char nombres[20][20];
 
 //Structura de nodo
 struct Nodo{
-    int info;
+    char info;
     struct Nodo *sig;
 };
 typedef struct Nodo tNodo;
@@ -31,12 +31,146 @@ Lista Lista_CREA_NODO(char valor)
     }
     else
     {
-        system("cls");
        
     }
     return aux;
 }
+//-------------------------------------------------------------------
+Lista Lista_INSERTA_PRINCIPIO(Lista L, char x)
+{
+    Lista pNodo;
 
+    pNodo = Lista_CREA_NODO(x);
+    pNodo->sig = L;
+    L = pNodo;
+    pNodo = NULL;
+    return L;
+}
+Lista Lista_INSERTA_FINAL(Lista L, char x)
+{
+    Lista pNodo, aux;
+
+    pNodo = Lista_CREA_NODO(x);
+    if (L == NULL)
+        L = pNodo;
+    else
+    {
+        aux = L;
+        while (aux->sig != NULL)
+            aux = aux->sig;
+        aux->sig = pNodo;
+        aux = NULL;
+    }
+    pNodo = NULL;
+    return L;
+}
+Lista Lista_INSERTA_EN_POSICION(Lista L, char x, int pos)
+{
+    Lista pNodo, aux;
+    int i, largo;
+
+    largo = Lista_LARGO(L);
+    pNodo = Lista_CREA_NODO(x);
+    if (pos <= largo+1) //Si p es válido para el largo de la lista.
+    {
+        if (pos == 1)	//Inserta en la primera posición.
+            L = Lista_INSERTA_PRINCIPIO(L, x);
+        else
+        {
+            if (pos == largo+1)           //Inserta en la última posición.
+                L = Lista_INSERTA_FINAL(L, x);
+            else
+            {
+                aux = L;
+                i = 1;
+                while (i < pos-1)
+                {
+                    aux = aux->sig;
+                    i = i+1;
+                }
+                pNodo->sig = aux->sig;
+                aux->sig = pNodo;
+                aux = NULL;
+            }
+        }
+    }
+    pNodo = NULL;
+    return L;
+}
+
+Lista Lista_INSERTA_ORDENADO(Lista L, char x)
+{
+    Lista pNodo, aux1, aux2;
+
+    pNodo = Lista_CREA_NODO(x);
+    if (L == NULL)
+        L = pNodo;
+    else
+    {
+        if (x < L->info) //Inserta al principio de L.
+        {
+            pNodo->sig = L;
+            L = pNodo;
+        }
+        else
+        {		//2 ó más Nodos, o inserción al final.
+            aux1 = L;
+            while ((aux1 != NULL) && (aux1->info < x))
+            {
+                aux1 = aux1->sig;
+            }
+            aux2 = L;
+            while (aux2->sig != aux1)
+                aux2 = aux2->sig;
+            aux2->sig = pNodo;
+            pNodo->sig = aux1;
+            aux1 = NULL;
+            aux2 = NULL;
+        }
+    }
+    pNodo = NULL;
+    return L;
+}
+Lista Lista_ELIMINA(Lista L, int p)
+{
+    int cont = 1;
+    Lista aux, aux2;
+
+    aux = L;
+    if (p == 1)
+    {
+        L = L->sig;
+        aux->sig = NULL;
+        free(aux);
+    }
+    else
+    {
+        while (cont < p-1)
+        {
+            aux = aux->sig;
+            cont++;
+        }
+        aux2 = aux->sig;
+        aux->sig = aux2->sig;
+        aux2->sig = NULL;
+        free(aux2);
+    }
+    return L;
+}
+int Lista_LARGO(Lista L)
+{
+    Lista aux;
+    int cont = 0;
+
+    aux = L;
+    while(aux != NULL)
+    {
+        aux = aux->sig;
+        cont++;
+    }
+    return cont;
+}
+//-------------------------------------------------------------
 void Lista_IMPRIME(Lista L)
 {
     Lista aux;
@@ -51,6 +185,7 @@ void Lista_IMPRIME(Lista L)
     printf("NULL");
 
 }
+//-------------------------------------------------------------
 
 
 void escribirResultados(const char *nombre_archivo, char *info){
@@ -69,6 +204,12 @@ void escribirResultados(const char *nombre_archivo, char *info){
   printf("Se escribio en el archivo!");
 }
 
+// Almacenamiento.
+void almacenarRuts(char *token){
+  printf("rut %s", token);
+}
+
+// Manejo de archivos.
 void leerProblema(const char *nombre_archivo){
     FILE * archivo;
     char linea[1000],
@@ -85,7 +226,8 @@ void leerProblema(const char *nombre_archivo){
     while( fgets (linea, 1000, archivo) != NULL ){
         token = strtok(linea, delimitador);
         printf("RUT: %s\n", token);
-        
+        almacenarRuts(token);
+
         token = strtok(NULL, delimitador);
         printf("NOMBRE: %s\n", token);
         
@@ -111,30 +253,30 @@ char *ingresoArchivo(int a){
   return file;
 }
 
+// __init__
 int main(void){
   // Variables locales.
   char *archivo = (char *)malloc(1);
   char *archivoSalida = (char *)malloc(1);
 
-  // Calls
   archivo = ingresoArchivo(0);
   archivoSalida = ingresoArchivo(1);
-  // leerProblema(archivo);  // input.csv
+  leerProblema(archivo);  // input.csv
+  
 
   // Escritura de archivos.
-  escribirResultados(archivoSalida, "Manolo estuvo aqui!");
+  // escribirResultados(archivoSalida, "Manolo estuvo aqui!");
 
-   
-   
-   
-   
-   //funcion lista enlazada
- /* Lista L;
-  L = Lista_INICIALIZA();
-  valor = 'c';
-  pos = LeePosicion();
-      if (pos <= Lista_LARGO(L)+1)
-                L = Lista_INSERTA_EN_POSICION(L, valor, pos);*/
+  //funcion lista enlazada  (creacion de lista )
+  // Lista L;
+  // L = Lista_INICIALIZA();
+  // char valor = 'a';
+  // int pos = 1;
+  // if (pos <= Lista_LARGO(L)+1){
+  //     L = Lista_INSERTA_EN_POSICION(L, valor, pos);
+  // }
+  
+  // Lista_IMPRIME(L);
 
 
 
