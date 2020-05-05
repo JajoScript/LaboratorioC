ae090a6cefa890f0d56d35787d5522a4cd6a23f9
Javier Almarza <javieralmarzabucarey@gmail.com>
2020-05-05 02:38:22 +0000

    tipo de dato int para valor

--- main.c
+++ main.c
@@ -43,10 +43,147 @@ Lista Lista_INSERTA_PRINCIPIO(Lista L, int valor){
     return L;
 };
 
+Lista Lista_INSERTA_FINAL(Lista L, int valor){
+    Lista pNodo, aux;
+
+    pNodo = Lista_CREA_NODO(valor);
+    if (L == NULL)
+        L = pNodo;
+    else
+    {
+        aux = L;
+        while (aux->sig != NULL)
+            aux = aux->sig;
+        aux->sig = pNodo;
+        aux = NULL;
+    }
+    pNodo = NULL;
+    return L;
+}
+
+int Lista_LARGO(Lista L){
+    
+    Lista aux;
+    int cont = 0;
+    aux = L;
+    while(aux != NULL){
+        aux = aux->sig;
+        cont++;
+    }
+    return cont;
+}
+
+Lista Lista_INSERTA_ORDENADO(Lista L, int valor){
+    
+    Lista pNodo, aux1, aux2;
+    pNodo = Lista_CREA_NODO(valor);
+    if (L == NULL)
+        L = pNodo;
+    else
+    {
+        if (valor < L->info) //Inserta al principio de L.
+        {
+            pNodo->sig = L;
+            L = pNodo;
+        }
+        else
+        {		//2 ó más Nodos, o inserción al final.
+            aux1 = L;
+            while ((aux1 != NULL) && (aux1->info < valor))
+            {
+                aux1 = aux1->sig;
+            }
+            aux2 = L;
+            while (aux2->sig != aux1)
+                aux2 = aux2->sig;
+            aux2->sig = pNodo;
+            pNodo->sig = aux1;
+            aux1 = NULL;
+            aux2 = NULL;
+        }
+    }
+    pNodo = NULL;
+    return L;
+}
+
+Lista Lista_INSERTA_EN_POSICION(Lista L, int valor, int posicion){
+  Lista pNodo, aux;
+  int i, largo;
+
+  largo = Lista_LARGO(L);
+  pNodo = Lista_CREA_NODO(valor);
+    
+  if (posicion <= largo+1){ //Si p es válido para el largo de la lista.
+    if (posicion == 1){ //Inserta en la primera posición.
+      L = Lista_INSERTA_PRINCIPIO(L, valor); 
+    }	else {
+      if (posicion == largo+1){ //Inserta en la última posición.
+        L = Lista_INSERTA_FINAL(L, valor);
+      } else {
+        aux = L;
+        i = 1;
+          
+        while (i < posicion-1){
+          aux = aux->sig;
+          i = i+1;
+        }
+          
+        pNodo->sig = aux->sig;
+        aux->sig = pNodo;
+        aux = NULL;
+      }
+    }
+  }
+
+  pNodo = NULL;
+  return L;
+};
+
+Lista Lista_ELIMINA(Lista L, int p){
+    int cont = 1;
+    Lista aux, aux2;
+
+    aux = L;
+    if (p == 1){
+      L = L->sig;
+      aux->sig = NULL;
+      free(aux);
+    } else {
+      while (cont < p-1) {
+        aux = aux->sig;
+        cont++;
+      }
+      
+      aux2 = aux->sig;
+      aux->sig = aux2->sig;
+      aux2->sig = NULL;
+      free(aux2);
+    }
+
+    return L;
+};
+
+void Lista_IMPRIME(Lista L){
+  Lista aux;
+  aux = L;
+    
+  printf("\n\n\tL -> ");
+  while(aux != NULL){
+    printf("%d -> ", aux->info);
+    aux = aux->sig;
+  };
+
+  printf("NULL");
+}
+
+
+
 
 // Manejo de datos.
 void capturarRut(char *token){
+ 
   printf("\nSe Capturo el rut: %s \n", token);
+  
 
 }
 
@@ -135,112 +272,12 @@ int main(void){
 
 
 
-Lista Lista_INSERTA_FINAL(Lista L, char x)
-{
-    Lista pNodo, aux;
-
-    pNodo = Lista_CREA_NODO(x);
-    if (L == NULL)
-        L = pNodo;
-    else
-    {
-        aux = L;
-        while (aux->sig != NULL)
-            aux = aux->sig;
-        aux->sig = pNodo;
-        aux = NULL;
-    }
-    pNodo = NULL;
-    return L;
-}
 
 
-Lista Lista_ELIMINA(Lista L, int p)
-{
-    int cont = 1;
-    Lista aux, aux2;
 
-    aux = L;
-    if (p == 1)
-    {
-        L = L->sig;
-        aux->sig = NULL;
-        free(aux);
-    }
-    else
-    {
-        while (cont < p-1)
-        {
-            aux = aux->sig;
-            cont++;
-        }
-        aux2 = aux->sig;
-        aux->sig = aux2->sig;
-        aux2->sig = NULL;
-        free(aux2);
-    }
-    return L;
-}
-int Lista_LARGO(Lista L)
-{
-    Lista aux;
-    int cont = 0;
 
-    aux = L;
-    while(aux != NULL)
-    {
-        aux = aux->sig;
-        cont++;
-    }
-    return cont;
-}
 
-Lista Lista_INSERTA_EN_POSICION(Lista L, char x, int pos)
-{
-    Lista pNodo, aux;
-    int i, largo;
 
-    largo = Lista_LARGO(L);
-    pNodo = Lista_CREA_NODO(x);
-    if (pos <= largo+1) //Si p es válido para el largo de la lista.
-    {
-        if (pos == 1)	//Inserta en la primera posición.
-            L = Lista_INSERTA_PRINCIPIO(L, x);
-        else
-        {
-            if (pos == largo+1)           //Inserta en la última posición.
-                L = Lista_INSERTA_FINAL(L, x);
-            else
-            {
-                aux = L;
-                i = 1;
-                while (i < pos-1)
-                {
-                    aux = aux->sig;
-                    i = i+1;
-                }
-                pNodo->sig = aux->sig;
-                aux->sig = pNodo;
-                aux = NULL;
-            }
-        }
-    }
-    pNodo = NULL;
-    return L;
-}
 
-void Lista_IMPRIME(Lista L)
-{
-    Lista aux;
 
-    aux = L;
-    printf("\n\n\tL -> ");
-    while(aux != NULL)
-    {
-        printf("%s -> ", aux->info);
-        aux = aux->sig;
-    }
-    printf("NULL");
-
-}
 */
